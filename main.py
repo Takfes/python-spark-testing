5aa4e8ccd0c37afaee7d98fda2b57bd3898940c9
tfe <Takfes@users.noreply.github.com>
2023-04-13 21:51:59 +0300

    add functionalities on spark dataframe

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
-import os
+import pandas as pd
+import pyspark.sql.functions as F
 from pyspark import SparkContext, SparkConf
-from pyspark.sql import SparkSession
-from pyspark.sql import SQLContext
+from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.functions import col, sum
+from pyspark.sql.types import (
+    StringType,
+    IntegerType,
+    FloatType,
+    DoubleType,
+    BooleanType,
+    DateType,
+    TimestampType,
+)
+from utils.sparkboost import SBFrame
 
 # settings
 FILE_PATH = "flights"
 FILE_NAME = "flights.csv"
-
-# rereview env variables
-{k: v for k, v in os.environ.items() if k.startswith("SPARK")}
 
 # http://localhost:4040/jobs/
 spark = SparkSession.builder.appName(f"{FILE_PATH}").enableHiveSupport().getOrCreate()
 
 # read data
 df = spark.read.csv(f"{FILE_PATH}/{FILE_NAME}", header=True, inferSchema=True)
+print(f"Shape {df.count(), len(df.columns)}")
 
-df.count()
-df.columns
-df.count(), len(df.columns)
-
-df.show(3)
-df.dtypes
+# check datatypes
 df.printSchema()
+df.dtypes
 
-df.agg(
-    *[sum(col(c).isNull().cast("int")).alias(c + "_null_count") for c in df.columns]
-).toPandas().T
+# set datatypes
+for col_name in ["FLIGHT_NUMBER"]:
+    df = df.withColumn(col_name, col(col_name).cast(StringType()))
 
-# TODO
-# unlock basic funcitonalities as insull or describe etc
-# work with MLLib
+# SBFrame usage
+sb = SBFrame(df, spark)
+
+sb.head()
+sb.isnull()
+sb.isnull(normalize=True)
+sb.dtypes
+sb.printSchema()
